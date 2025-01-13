143fedda790784ed2b2243baa891892ffa6c5d9d
amolovocom <amol.bhostekar@ovocom.in>
2025-01-13 11:04:11 +0530

    hearthover-fill

--- src/Components/Candidates/CandidateCard.tsx
+++ src/Components/Candidates/CandidateCard.tsx
@@ -55,7 +55,7 @@ const CandidateCard = () => {
                             </div>
                             <div className="tw-flex tw-items-center tw-justify-end date-div tw-mt-auto">
                                 <div className="tw-flex tw-gap-3">
-                                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="tw-w-5 tw-fill-[#7f56d9]" viewBox="0 0 24 24"><path d="m12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3" /></svg></a>
+                                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" className="heart-icon tw-w-7" viewBox="0 0 36 36"><path fill="none" stroke="#7f56d9" d="M31.885 13.764a7.66 7.66 0 0 0-7.66-7.661A7.65 7.65 0 0 0 18 9.309a7.65 7.65 0 0 0-6.224-3.206a7.66 7.66 0 0 0-7.661 7.661c0 .6.076 1.18.206 1.74C5.385 22.113 12.733 29.085 18 31c5.266-1.915 12.614-8.887 13.678-15.496c.131-.56.207-1.14.207-1.74"/></svg></a>
                                 </div>
                             </div>
                         </div>
